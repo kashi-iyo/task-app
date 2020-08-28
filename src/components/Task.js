35a19df9366aee5900f144b89ff870f4781f13b1
kasi-iyo <snakeman143@gmail.com>
2020-08-28 19:40:47 +0900

    Task.js作成

---
+++ src/components/Task.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Task = () => {
+    return (
+        <li>
+            <span>Task Title</span>
+            <div>
+                <button>
+                    削除
+                </button>
+                <button>
+                    編集
+                </button>
+            </div>
+        </li>
+    )
+}
+
+export default Task
