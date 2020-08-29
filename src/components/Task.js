b19abfd2a45bb8a5f94fbb5cf7e79033d8ca4b4e
kasi-iyo <snakeman143@gmail.com>
2020-08-29 15:31:33 +0900

    TaskコンポーネントにTask表示とイベント

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { TaskListContext } from '../context/TaskListContext'
 
-const Task = () => {
+const Task = ({ task }) => {
+    const { removeTask, findItem } = useContext(TaskListContext)
+
     return (
-        <li>
-            <span>Task Title</span>
-            <div>
-                <button>
+        <li className="list" >
+            <div className="task">{task.title}</div>
+            <div className="button">
+                <button onClick={() => removeTask(task.id)}>
                     削除
                 </button>
-                <button>
+                <button onClick={() => findItem(task.id)}>
                     編集
                 </button>
             </div>
