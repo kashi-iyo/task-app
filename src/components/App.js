f92fe1d757a2a47cd30752b758e5dbd807d07193
kasi-iyo <snakeman143@gmail.com>
2020-08-29 15:32:38 +0900

    App.js編集

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import TaskList from './TaskList'
+import TaskForm from './TaskForm'
+import Header from './Header'
 import TaskListContextProvider from '../context/TaskListContext'
+import '../App.css'
 
 const App = () => {
     return (
         <TaskListContextProvider>
-            <div>
+            <Header />
+            <div className="todo">
+                <TaskForm />
                 <TaskList />
             </div>
         </TaskListContextProvider>
